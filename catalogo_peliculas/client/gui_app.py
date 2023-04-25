80cb157216a4a370171e55116cacf79a37bc3bc8
bas-clavijo <bas.clavijo@duocuc.cl>
2023-04-25 19:18:46 -0400

    Agregando atributos de campo

--- catalogo_peliculas/client/gui_app.py
+++ catalogo_peliculas/client/gui_app.py
@@ -7,14 +7,19 @@ def barra_menu(root):
 
     #Se crean los objetos de la barra menu
     menu_inicio = tk.Menu(barra_menu, tearoff=0)
+    #Menu de inicio
     barra_menu.add_cascade(label='Inicio', menu= menu_inicio)
 
     menu_inicio.add_command(label='Crear registro en DB')
     menu_inicio.add_command(label='Eliminar registro en DB')
     menu_inicio.add_command(label='Salir', command=root.destroy)
 
-
-
+    #Menu Consultas
+    barra_menu.add_cascade(label='Consultas')
+    #Menu Configuracion
+    barra_menu.add_cascade(label='Configuracion')
+    #Menu Ayuda
+    barra_menu.add_cascade(label='Ayuda')
 
 #Se crea la clase frame
 class Frame(tk.Frame):
@@ -22,4 +27,38 @@ class Frame(tk.Frame):
         super().__init__(root,width=480, height=320 )
         self.root = root
         self.pack()
-        self.config(bg='green')
+        #self.config(bg='green')
+
+        self.campos_pelicula()
+
+    #Funcion campos de peliculas
+    def campos_pelicula(self):
+        #Labels de cada campo(Nombre)
+        self.label_nombre = tk.Label(self, text='Nombre:')
+        self.label_nombre.config(font= ('Arial', 12, 'bold'))
+        self.label_nombre.grid(row=0, column=0, padx=10, pady=10)
+
+        #Label para duracion
+        self.label_duracion = tk.Label(self, text='Duracion:')
+        self.label_duracion.config(font= ('Arial', 12, 'bold'))
+        self.label_duracion.grid(row=1, column=0, padx=10, pady=10)
+
+        #Label para genero
+        self.label_genero = tk.Label(self, text='Genero:')
+        self.label_genero.config(font= ('Arial', 12, 'bold'))
+        self.label_genero.grid(row=2, column=0, padx=10, pady=10)
+
+        #Entrys de cada campo(Nombre)
+        self.entry_nombre = tk.Entry(self)
+        self.entry_nombre.config(width=50, state='disabled',font= ('Arial', 12))
+        self.entry_nombre.grid(row=0, column=1, padx=10, pady=10)
+
+        #Entry para duracion
+        self.entry_duracion = tk.Entry(self)
+        self.entry_duracion.config(width=50, state='disabled',font= ('Arial', 12))
+        self.entry_duracion.grid(row=1, column=1, padx=10, pady=10)
+
+        #Entry para genero
+        self.entry_genero = tk.Entry(self)
+        self.entry_genero.config(width=50, state='disabled',font= ('Arial', 12))
+        self.entry_genero.grid(row=2, column=1, padx=10, pady=10)
