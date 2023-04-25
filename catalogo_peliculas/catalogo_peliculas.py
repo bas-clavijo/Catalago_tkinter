d4f6b300f8ed6362fa9e415fff301a911e2a6568
bas-clavijo <bas.clavijo@duocuc.cl>
2023-04-25 15:55:00 -0400

    Agregando icono

--- catalogo_peliculas/catalogo_peliculas.py
+++ catalogo_peliculas/catalogo_peliculas.py
@@ -5,6 +5,9 @@ import tkinter as tk
 
 def main():
     root = tk.Tk()
+    root.title('Catalogo de peliculas')
+    root.iconbitmap('img/logo.ico')
+    root.resizable(0,0)
     root.mainloop()
 
 if __name__ == '__main__':
